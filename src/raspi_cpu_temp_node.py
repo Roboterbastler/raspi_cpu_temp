4049f51821824e3e3fbde1863e3c8e0f8c05e683
Jacob Seibert <roboterbastler@posteo.de>
2017-03-12 10:13:56 +0100

    Add python node code

---
+++ src/raspi_cpu_temp_node.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+import rospy
+from raspi_cpu_temp.msg import CpuTemp
+
+def read_cpu_temp():
+    tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
+    cpu_temp = tempFile.read()
+    tempFile.close()
+    msg = CpuTemp()
+    msg.stamp = rospy.Time.now()
+    msg.temp = float(cpu_temp)/1000
+    return msg
+    
+def raspi_cpu_temp_node():
+    pub = rospy.Publisher('cpu_temp', CpuTemp, queue_size=10)
+    rospy.init_node('raspi_cpu_temp')
+    rate = rospy.Rate(1) # 1 Hz
+    
+    while not rospy.is_shutdown():
+        msg = read_cpu_temp()
+        pub.publish(msg)
+        rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        raspi_cpu_temp_node()
+    except rospy.ROSInterruptException:
+        pass
