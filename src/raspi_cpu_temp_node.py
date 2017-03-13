5633fefafdfc2972b1e18846f7bba9355841d7ba
Jacob Nikolai Emanuel Seibert <j.seibert@campus.tu-berlin.de>
2017-03-13 21:47:42 +0100

    Fix message

--- src/raspi_cpu_temp_node.py
+++ src/raspi_cpu_temp_node.py
@@ -2,7 +2,7 @@
 
 import os
 import rospy
-from raspi_cpu_temp.msg import CpuTemp
+from roseau_msgs.msg import CpuTemp
 
 def read_cpu_temp():
     tempFile = open( "/sys/class/thermal/thermal_zone0/temp" )
@@ -12,12 +12,12 @@ def read_cpu_temp():
     msg.stamp = rospy.Time.now()
     msg.temp = float(cpu_temp)/1000
     return msg
-    
+
 def raspi_cpu_temp_node():
     pub = rospy.Publisher('cpu_temp', CpuTemp, queue_size=10)
     rospy.init_node('raspi_cpu_temp')
     rate = rospy.Rate(1) # 1 Hz
-    
+
     while not rospy.is_shutdown():
         msg = read_cpu_temp()
         pub.publish(msg)
